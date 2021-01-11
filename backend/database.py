814aa49c209a9518b013395ed5a48241dddd891c
jane-web-dev <jennimyau@gmail.com>
2021-01-11 23:54:19 +0300

    hmm

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://s265099:nfn012@localhost/studs"
+USERNAME = ""
+PASSWORD = ""
+DB = ""
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@localhost/{DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
